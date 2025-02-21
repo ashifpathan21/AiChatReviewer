3c1cbaab67dc68b18924572b3a6db8a5ae0673c6
Ashif Pathan <ashifbgmiking@gmail.com>
2025-02-21 08:26:15 +0530

    Update Main.jsx

--- FRONTEND/src/components/Main.jsx
+++ FRONTEND/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React , {useState , useEffect} from 'react'
+ import React , {useState , useEffect} from 'react'
 import 'prismjs/themes/prism-tomorrow.css'
 import prism from 'prismjs'
 import axios from 'axios'
@@ -30,8 +30,8 @@ setLoading(false)
     
 
   return (
-    <div className='h-screen flex p-3 justify-between w-screen'>
-      <div className='relative h-full w-[49.5%] bg-black border-2 rounded-lg border-gray-600'>
+    <div className='h-screen flex p-3 sm:flex-col-reverse justify-between w-screen'>
+      <div className='relative md:h-full lg:h-full sm:h-[49.5%] sm:w-full md:w-[49.5%] lg:w-[49.5%] w-[49.5%] bg-black border-2 rounded-lg border-gray-600'>
     
     <Editor 
     value={code} 
@@ -45,13 +45,13 @@ setLoading(false)
 
     <button
     onClick={review}
-    className='rounded-lg mr-2 mb-2 font-semibold absolute bottom-0 right-0 bg-white p-3 text-black '>
+    className='rounded-lg mr-2 mb-2 font-semibold absolute sm:mb-8 bottom-0 right-0 bg-white p-3 text-black '>
         Review
     </button>
 
       </div>
 
-      <div className='h-full text-white w-[49.5%]  overflow-scroll px-3 py-2 bg-gray-600 border-2 rounded-lg border-black'>
+      <div className='md:h-full lg:h-full sm:h-[49.5%] sm:w-full text-white md:w-[49.5%] lg:w-[49.5%] overflow-scroll px-3 py-2 bg-gray-600 border-2 rounded-lg border-black'>
      {loading ? <Loading /> : (
                     <Markdown rehypePlugins={[rehypeHighlight]}>
                         {output}
